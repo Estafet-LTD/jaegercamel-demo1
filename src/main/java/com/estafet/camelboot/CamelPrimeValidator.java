6b5d051d63a4e970057d205e3c04ac0a403c9e43
Ivo Costa <spacemanpt@gmail.com>
2017-09-28 16:32:53 +0100

    Added logging

--- src/main/java/com/estafet/camelboot/CamelPrimeValidator.java
+++ src/main/java/com/estafet/camelboot/CamelPrimeValidator.java
@@ -8,6 +8,7 @@ public class CamelPrimeValidator extends RouteBuilder {
     @Override
     public void configure() throws Exception {
         from("netty4-http:http://0.0.0.0:8080/isPrime")
+                .to("log:foo")
                 .process("validateService");
     }
 }

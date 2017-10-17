bdabfbbefeebdbe6b7b9ee156128b8c3f59a535c
Ivo Costa <ivo.costa@estafet.com>
2017-10-17 14:51:13 +0100

    Added logging to processor

--- src/main/java/com/estafet/camelboot/ValidateService.java
+++ src/main/java/com/estafet/camelboot/ValidateService.java
@@ -7,6 +7,7 @@ import io.opentracing.propagation.Format;
 import org.apache.camel.Exchange;
 import org.apache.camel.Message;
 import org.apache.camel.Processor;
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -17,17 +18,22 @@ public class ValidateService implements Processor {
     @Autowired
     private Tracer tracer;
 
+    private static Logger log = Logger.getLogger(ValidateService.class);
+
     @Override
     public void process(Exchange exchange) throws Exception {
+        log.info("processor start");
         Message message = exchange.getIn();
         Map<String, Object> headers = message.getHeaders();
         String header = headers.get("CamelHttpQuery").toString();
 
+        log.debug("debugging");
         int num = Integer.parseInt(header.substring(header.indexOf("=") + 1));
 
         SpanContext spanContext = tracer.extract(Format.Builtin.HTTP_HEADERS, new ObjectMapExtractAdapter(headers));
         Span span = tracer.buildSpan("ValidatePrime").asChildOf(spanContext).withTag("number", num).startManual();
 
+        log.error("error faked");
         boolean prime = num > 1;
 
         for (int x = 2; x < num && prime; x++) {
@@ -35,6 +41,7 @@ public class ValidateService implements Processor {
         }
         exchange.getOut().setBody("{\"result\": " + prime + "}");
 
+        log.info("finished");
         span.finish();
     }
 }

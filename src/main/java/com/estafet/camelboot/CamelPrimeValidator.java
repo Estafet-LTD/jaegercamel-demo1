0c94aa6b1969a4477c8b353af517562bf11e21d7
Ivo Costa <ivo.costa@estafet.com>
2017-10-17 02:01:20 +0100

    rabbitmq experiment

--- src/main/java/com/estafet/camelboot/CamelPrimeValidator.java
+++ src/main/java/com/estafet/camelboot/CamelPrimeValidator.java
@@ -11,6 +11,6 @@ public class CamelPrimeValidator extends RouteBuilder {
         from("netty4-http:http://0.0.0.0:8080/isPrime")
                 .to("log:foo")
                 .process("validateService")
-                .to("rabbitmq://" + rabbitmqUrl + "/");
+                .to("rabbitmq://" + rabbitmqUrl + "/amq.direct");
     }
 }

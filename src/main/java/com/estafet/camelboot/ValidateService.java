1dfa7fa43cd2a82311134956d347d6c327b7c131
Ivo Costa <ivo.costa@estafet.com>
2017-10-19 10:19:41 +0100

    clean up on dev

--- src/main/java/com/estafet/camelboot/ValidateService.java
+++ src/main/java/com/estafet/camelboot/ValidateService.java
@@ -18,30 +18,23 @@ public class ValidateService implements Processor {
     @Autowired
     private Tracer tracer;
 
-    private static Logger log = Logger.getLogger(ValidateService.class);
-
     @Override
     public void process(Exchange exchange) throws Exception {
-        log.info("processor start");
         Message message = exchange.getIn();
         Map<String, Object> headers = message.getHeaders();
         String header = headers.get("CamelHttpQuery").toString();
 
-        log.debug("debugging");
         int num = Integer.parseInt(header.substring(header.indexOf("=") + 1));
 
         SpanContext spanContext = tracer.extract(Format.Builtin.HTTP_HEADERS, new ObjectMapExtractAdapter(headers));
         Span span = tracer.buildSpan("ValidatePrime").asChildOf(spanContext).withTag("number", num).startManual();
 
-        log.error("error faked");
         boolean prime = num > 1;
 
         for (int x = 2; x < num && prime; x++) {
             prime = (num % x) != 0;
         }
         exchange.getOut().setBody("{\"result\": " + prime + "}");
-
-        log.info("finished");
         span.finish();
     }
 }
